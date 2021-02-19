56fd730a87aba5a3c8a6dc144731e759a39deea7
Rafael Cavalcanti <rafael240519@gmail.com>
2021-02-19 06:22:34 -0300

    corrigindo tamanho do codigo do link para videos do vimeo

--- app/models/aula.rb
+++ app/models/aula.rb
@@ -1,5 +1,5 @@
 class Aula < ApplicationRecord
   validates :nomeAula, presence: true, length: {minimum: 5}
-  validates :conteudo, presence: true, length: {minimum: 9}
+  validates :conteudo, presence: true, length: {minimum: 7}
   belongs_to :curso
 end
